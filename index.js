c23732b9d5226eeac6b4dc1c3c691abe8b0885bc
Anisa Monger <linnus2004@gmail.com>
2025-02-28 16:37:25 -0500

    1st commit to my ALAB30831

---
+++ index.js
@@ -0,0 +1,28 @@
+// // make a for loop 1 to 100
+// for (let i = 1; i < 100; i ++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz")
+//   } else if (i % 5 === 0) {
+// console.log("Buzz")
+//   } else {
+// console.log(i)
+//   }
+     
+    
+//     }
+
+// Part 2
+// A.
+// for (let n = 3; n < 100; n ++) {
+//   if (n % 2 !== 0 && n % 3 !== 0 && n % n == 0) {
+//     console.log(n)  
+//     break;
+//   } 
+  
+// } 
+
+// part 3
+//Split Method
+// split twice
