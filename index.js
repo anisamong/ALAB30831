82996c3f778fb5931b96fcf049eb58e618d070e4
Anisa Monger <linnus2004@gmail.com>
2025-03-04 13:52:27 -0500

    Additional changes made to this file

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
-// // make a for loop 1 to 100
-for (let i = 1; i < 100; i ++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
-  } else if (i % 3 === 0) {
-    console.log("Fizz")
-  } else if (i % 5 === 0) {
-console.log("Buzz")
-  } else {
-console.log(i)
-  }
+// // Part 1
+// //  make a for loop 1 to 100
+// for (let i = 1; i < 100; i ++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz")
+//   } else if (i % 5 === 0) {
+// console.log("Buzz")
+//   } else {
+// console.log(i)
+//   }
      
     
-    }
+//     }
 
-// Part 2
-A.
-for (let n = 5; n < 100; n ++) {
-  if ( n > 1 && n % 2 !== 0 && n % 3 !== 0 && n % n == 0) {
-    console.log(n)  
-    break;
-  } 
-} 
+// // Part 2
+
+// for (let n = 5; n < 100; n ++) {
+//   if ( n > 1 && n % 2 !== 0 && n % 3 !== 0 && n % n == 0) {
+//     console.log(n)  
+//     break;
+//   } 
+// } 
 
 
 // part 3
 
-
-const csvVariable = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
+const csvVariable = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
 const rows = csvVariable.split('\n');
-
-const cells = [];
-for (let i = 0; i < rows.length; i++) {
-  cells.push(rows[i].split(','));
-}
-
-console.log(cells);
+const cells = csvVariable.split(',');
+console.log(rows);
