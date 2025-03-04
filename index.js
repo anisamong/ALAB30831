66be82a8740c4204b1f57fae684d2073bd685cec
Anisa Monger <linnus2004@gmail.com>
2025-03-04 13:54:45 -0500

    Uncommented my codes

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-// // Part 1
-// //  make a for loop 1 to 100
-// for (let i = 1; i < 100; i ++) {
-//   if (i % 3 === 0 && i % 5 === 0) {
-//     console.log("FizzBuzz");
-//   } else if (i % 3 === 0) {
-//     console.log("Fizz")
-//   } else if (i % 5 === 0) {
-// console.log("Buzz")
-//   } else {
-// console.log(i)
-//   }
+// Part 1
+//  make a for loop 1 to 100
+for (let i = 1; i < 100; i ++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz")
+  } else if (i % 5 === 0) {
+console.log("Buzz")
+  } else {
+console.log(i)
+  }
      
     
-//     }
+    }
 
-// // Part 2
+// Part 2
 
-// for (let n = 5; n < 100; n ++) {
-//   if ( n > 1 && n % 2 !== 0 && n % 3 !== 0 && n % n == 0) {
-//     console.log(n)  
-//     break;
-//   } 
-// } 
+for (let n = 5; n < 100; n ++) {
+  if ( n > 1 && n % 2 !== 0 && n % 3 !== 0 && n % n == 0) {
+    console.log(n)  
+    break;
+  } 
+} 
 
 
-// part 3
+part 3
 
 const csvVariable = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
 const rows = csvVariable.split('\n');
